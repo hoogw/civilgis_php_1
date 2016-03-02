e78080e800590ea3ed15d405425f2ff90d321422
hoogw <hoogw@hotmail.com>
2016-03-02 15:43:01 -0800

    nyc zoing

---
+++ public/js/classification/area/New_York_Manhattan/Zoning.js
@@ -0,0 +1,174 @@
+
+
+_code_column_name = 'ZoneDist1';
+
+
+_designation['New_York_Manhattan_Zoning'] = [
+
+
+
+
+
+    { 'code': 'BPC', 'description': 'SPECIAL BATTERY PARK CITY DISTRICT', 'color': "aqua" },
+    
+   { 'code': 'C1-6', 'description': 'Commercial', 'color': "aquamarine" },
+   { 'code': 'C1-6A', 'description': 'Commercial', 'color': "aquamarine" },
+   { 'code': 'C1-7', 'description': 'Commercial', 'color': "aquamarine" },
+   { 'code': 'C1-7A', 'description': 'Commercial', 'color': "aquamarine" },
+ { 'code': 'C1-8', 'description': 'Commercial', 'color': "aquamarine" },
+   { 'code': 'C1-8A', 'description': 'Commercial', 'color': "aquamarine" },
+   { 'code': 'C1-8X', 'description': 'Commercial', 'color': "aquamarine" },
+ { 'code': 'C1-9', 'description': 'Commercial', 'color': "aquamarine" },
+   { 'code': 'C1-9A', 'description': 'Commercial', 'color': "aquamarine" },
+   
+   
+   
+   
+   
+ 
+     { 'code': 'C2-6', 'description': 'Commercial', 'color': "blue" },
+     { 'code': 'C2-6A', 'description': 'Commercial', 'color': "blue" },
+     { 'code': 'C2-7', 'description': 'Commercial', 'color': "blue" },
+     { 'code': 'C2-7A', 'description': 'Commercial', 'color': "blue" },
+     { 'code': 'C2-8', 'description': 'Commercial', 'color': "blue" },
+     { 'code': 'C2-8A', 'description': 'Commercial', 'color': "blue" },
+     
+     
+     
+    { 'code': 'C4-2F', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-4', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-4A', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-4D', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-5', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-5A', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-5X', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-6', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-6A', 'description': 'Commercial', 'color': "blueviolet" },
+    { 'code': 'C4-7', 'description': 'Commercial', 'color': "blueviolet" },
+   
+    
+     { 'code': 'C5-1', 'description': "Commercial", 'color': "brown" },
+     { 'code': 'C5-1A', 'description': "Commercial", 'color': "brown" },
+    { 'code': 'C5-2', 'description': "Commercial", 'color': "brown" },
+    { 'code': 'C5-2.5', 'description': "Commercial", 'color': "brown" },
+    { 'code': 'C5-3', 'description': "Commercial", 'color': "brown" },
+    { 'code': 'C5-5', 'description': "Commercial", 'color': "brown" },
+    { 'code': 'C5-P', 'description': "Commercial", 'color': "brown" },
+    
+    
+    { 'code': 'C6-1', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-1G', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-2', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-2A', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-2G', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-2M', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-3', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-3A', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-3X', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-4', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-4.5', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-4A', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-4M', 'description': "Commercial", 'color': "chocolate" },
+    { 'code': 'C6-4X', 'description': "Commercial", 'color': "chocolate" },
+   
+    
+    { 'code': 'C6-5', 'description': 'Commercial', 'color': "coral" },
+    { 'code': 'C6-5.5', 'description': 'Commercial', 'color': "coral" },
+    { 'code': 'C6-6', 'description': 'Commercial', 'color': "coral" },
+    { 'code': 'C6-6.5', 'description': 'Commercial', 'color': "coral" },
+    { 'code': 'C6-7', 'description': 'Commercial', 'color': "coral" },
+    { 'code': 'C6-7T', 'description': 'Commercial', 'color': "coral" },
+    { 'code': 'C6-9', 'description': 'Commercial', 'color': "coral" },
+    
+    
+    { 'code': 'C8-3', 'description': 'Commercial', 'color': "firebrick" },
+    { 'code': 'C8-4', 'description': 'Commercial', 'color': "firebrick" },
+    
+    
+    
+    
+    
+    { 'code': 'M1-1', 'description': 'Manufacturing', 'color': "fuchsia" },
+    { 'code': 'M1-2', 'description': 'Manufacturing', 'color': "fuchsia" },
+    { 'code': 'M1-4', 'description': 'Manufacturing', 'color': "fuchsia" },
+    { 'code': 'M1-6', 'description': 'Manufacturing', 'color': "fuchsia" },
+    { 'code': 'M1-6D', 'description': 'Manufacturing', 'color': "fuchsia" },
+    
+    
+    
+    
+    
+    
+    
+    
+    { 'code': 'PARK', 'description': 'Park', 'color': "green" },
+    
+    
+    
+    { 'code': 'M1-5', 'description': 'Manufacturing', 'color': "hotpink" },
+    { 'code': 'M1-5/R7-2', 'description': 'Manufacturing', 'color': "hotpink" },
+    { 'code': 'M1-5/R7X', 'description': 'Manufacturing', 'color': "hotpink" },
+    { 'code': 'M1-5A', 'description': 'Manufacturing', 'color': "hotpink" },
+    { 'code': 'M1-5B', 'description': 'Manufacturing', 'color': "hotpink" },
+    { 'code': 'M1-5M', 'description': 'Manufacturing', 'color': "hotpink" },
+    
+    
+    
+    
+    { 'code': 'M2-1', 'description': 'Manufacturing', 'color': "indigo" },      
+   { 'code': 'M2-2', 'description': 'Manufacturing', 'color': "indigo" },   
+   { 'code': 'M2-3', 'description': 'Manufacturing', 'color': "indigo" },   
+   { 'code': 'M2-4', 'description': 'Manufacturing', 'color': "indigo" },   
+   
+   
+   
+     { 'code': 'M3-1', 'description': 'Manufacturing', 'color': "lime" },
+      { 'code': 'M3-2', 'description': 'Manufacturing', 'color': "lime" },
+     
+     
+     
+     { 'code': 'R10', 'description': 'Residence', 'color': "lightblue" },
+     { 'code': 'R10A', 'description': 'Residence', 'color': "lightblue" },
+     { 'code': 'R10H', 'description': 'Residence', 'color': "lightblue" },
+     
+     
+     
+     
+     
+     { 'code': 'R3-2', 'description': 'Residence', 'color': "olive" },
+     
+     
+     { 'code': 'R5', 'description': 'Residence', 'color': "orange" },
+        
+        
+     { 'code': 'R6', 'description': 'Residence', 'color': "purple" },   
+     { 'code': 'R6A', 'description': 'Residence', 'color': "purple" },
+     
+     
+     { 'code': 'R7-1', 'description': 'Residence', 'color': "papayawhip" },
+     { 'code': 'R7-2', 'description': 'Residence', 'color': "papayawhip" },
+     
+     
+     { 'code': 'R7A', 'description': "Residence", 'color': "peachpuff" }, 
+     { 'code': 'R7B', 'description': "Residence", 'color': "peachpuff" },
+     { 'code': 'R7D', 'description': "Residence", 'color': "peachpuff" },
+     { 'code': 'R7X', 'description': "Residence", 'color': "peachpuff" },
+     
+     
+     { 'code': 'R8', 'description': 'Residence', 'color': "plum" },
+    { 'code': 'R8A', 'description': 'Residence', 'color': "plum" },
+    { 'code': 'R8B', 'description': 'Residence', 'color': "plum" },
+    { 'code': 'R8X', 'description': 'Residence', 'color': "plum" },
+    
+    
+    
+    { 'code': 'R9', 'description': 'Residence', 'color': "red" },
+    { 'code': 'R9A', 'description': 'Residence', 'color': "red" },
+    { 'code': 'R9X', 'description': 'Residence', 'color': "red" },
+    
+    
+     { 'code': 'ZNA', 'description': ' ', 'color': "silver" }
+    
+   
+];
+
