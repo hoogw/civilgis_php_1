b97a7f8934837be919c90b44f2facba397908335
hoogw <hoogw@hotmail.com>
2016-09-01 16:04:36 -0700

    google play badge

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -13,7 +13,7 @@
 			<li><a href="http://transparentgov.net"><i class="fa fa-windows"></i> .Net</a></li>
 			<li><a href="http://phpmap.transparentgov.net"> PHP</a></li>
                         <li><a href="#"><i class="fa fa-apple"></i> ios</a></li>
-                        <li><a href="#"><i class="fa fa-android"></i> Android </a></li>
+                        <li><a href='https://play.google.com/store/apps/details?id=net.transparentgov.android.trgv_1&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img style= "height:80px;" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a></li>
                         <li><a href="#"> Java </a></li>
 		</ol>
 	</div>
--- application/view/_templates/index_side_bar.php
+++ application/view/_templates/index_side_bar.php
@@ -226,6 +226,22 @@
                                 
                                  <hr>
                                 
+                                 
+                                 
+                                 
+                                 <li class="dropdown">
+                                     
+					<a href='https://play.google.com/store/apps/details?id=net.transparentgov.android.trgv_1&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                                            <img style= "height:80px;" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/>
+                                        </a>
+					
+				</li>
+                                 
+                               
+                                 
+                                 
+                                 
+                                 <hr>
                                 <li class="dropdown">
 					<a href="#" class="dropdown-toggle">
 						<img src="<?php echo Config::get('URL'); ?>public/images/logo/phone.png" alt=""/> 
